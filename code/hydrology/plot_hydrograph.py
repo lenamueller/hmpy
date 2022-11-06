451c3a895c077db5b10980e1dee3b22141cbf032
Lena Mueller <lenamariemue@gmail.com>
2022-11-06 14:37:53 +0100

    Add plot of summation and duration curve, principal values

--- code/hydrology/plot_hydrograph.py
+++ code/hydrology/plot_hydrograph.py
@@ -1,37 +1,59 @@
+import math 
 import numpy as np 
 import matplotlib.pyplot as plt
 import pandas as pd 
 import datetime 
 import proplot as pplt
+import sys
 
 
+def remove_nan_from_list(list):
+    return [x for x in list if x is not np.NaN]
+
+def remove_inf_from_list(list):
+    return [x for x in list if x is not np.Inf]
+
+def clean_nan_inf(list):
+    return remove_inf_from_list(remove_nan_from_list(list))
+
+def clean_nan_inf_2_rows(list1, list2):    
+    list1_new, list2_new = [], []
+    for i in range(len(list1)):
+        if list1[i] not in [np.NaN, np.Inf] and list2[i] not in [np.NaN, np.Inf]:
+            list1_new.append(list1[i])
+            list2_new.append(list2[i])
+    return list1_new, list2_new
+    
 def start_date(dt):
     """Get earliest datetime from a list of datetime.datetime objects."""
-    try:
+    dt = clean_nan_inf(dt)
+    if dt:
         return min(dt)
-    except:
-        raise ValueError("Empty list has no min().")
+    else:
+        raise ValueError("start_date argument dt is empty")
 
 def end_date(dt):
     """Get latest datetime from a list of datetime.datetime objects."""
-    try: 
+    dt = clean_nan_inf(dt)
+    if dt: 
         return max(dt)
-    except:
-        raise ValueError("Empty list has no max().")
+    else:
+        raise ValueError("end_date argument dt is empty")
 
 def nb_days(dt):
     """Return number of given days from a list of datetime.datetime objects 
     and the number of days between the earliest and latest datetime.datetime object."""
     
+    dt = clean_nan_inf(dt)
+    
     nb_days_given = len(dt)
-    delta = end_date(dt) - start_date(dt)
-    nb_days_true = int(delta.days) +1
-    
-    if nb_days_given != nb_days_true:
-        print("Data contains {} days plus {} missing days".format(
-            nb_days_given, nb_days_true-nb_days_given))
-    
-    return nb_days_given, nb_days_true
+
+    if nb_days_given==0:
+        return 0,0
+    else:
+        delta = end_date(dt) - start_date(dt)
+        nb_days_true = int(delta.days) +1
+        return nb_days_given, nb_days_true
 
 def create_datetime_list(start_date, end_date):
     """Return a list of datetime.datetime objects from given start and end 
@@ -52,32 +74,145 @@ def start_and_end_of_data_gaps(missing_days):
     
     missing_days = sorted(missing_days)
     diff_list = []
-    
-    start = missing_days[0]
-    for i in np.arange(1, len(missing_days), 1):
-        delta = missing_days[i]-missing_days[i-1]
-        diff = delta.days
-        if diff>1:
-            diff_list.append((start,missing_days[i-1]))
-            start = missing_days[i]            
-    diff_list.append((start, missing_days[-1]))
-    
-    return diff_list
+    if missing_days != []:
+        start = missing_days[0]
+        for i in np.arange(1, len(missing_days), 1):
+            delta = missing_days[i]-missing_days[i-1]
+            diff = delta.days
+            if diff>1:
+                diff_list.append((start,missing_days[i-1]))
+                start = missing_days[i]            
+        diff_list.append((start, missing_days[-1]))
+        
+        return diff_list
+    else:
+        return []
 
-def plot(fn, dt, dc):
-    """Plot the hydrograph with data gaps from a list of discharge values (float)
-    in m^3/s and datetime.datetime objects"""
+def _cumulate(var):
+    """Return cumulated time series."""
+    # TODO: Should nan be replaced by zero?
+    if np.inf in var:
+        raise ValueError("List contains inf.")                
+    else:
+        if np.nan in var:
+            var = [0 if math.isnan(x) else x for x in var]
+        else:
+            pass
+        return np.cumsum(var)    
+
+def subset_timeframe(dt,var, first_date, last_date):
+    """Return sub lists of datetime.datetime objects and variable values 
+     between two dates (included). """
+     
+    dt, var = clean_nan_inf_2_rows(dt, var)
+    df = pd.DataFrame(list(zip(dt, var)), columns =['date', 'var'])
+    
+    after_start_date = df["date"] >= first_date
+    before_end_date = df["date"] <= last_date
+    df_sub = df.loc[after_start_date & before_end_date]
+    
+    dt_sub = df_sub["date"].tolist()
+    var_sub = df_sub["var"].tolist()
+    
+    return dt_sub, var_sub
+
+def _list_of_doy_between_two_dates(date_start, date_end):
+    doy_begin = date_start.timetuple().tm_yday
+    doy_end = date_end.timetuple().tm_yday
+    if doy_end >= doy_begin:
+        doy_of_interest = np.arange(doy_begin, doy_end+1, 1)
+    else:
+        first_part = np.arange(doy_begin, 366, 1)
+        second_part = np.arange(1, doy_end+1, 1)
+        doy_of_interest = list(first_part) + list(second_part)
+    
+    return doy_of_interest
+
+def subset_period(dt, var, period_begin, period_end):
+    doy = [x.timetuple().tm_yday for x in dt]
+    doy_of_interest = _list_of_doy_between_two_dates(period_begin, period_end)
+    
+    dt_sub, var_sub = [], []
+    for i in range(len(dt)):
+        doy_i = doy[i]
+        if doy_i in doy_of_interest:
+            dt_sub.append(dt[i])
+            var_sub.append(var[i])
+
+    return dt_sub, var_sub
+
+def principal_values(dt, va, period_begin, period_end, timeframe_begin, timeframe_end):
+    """Return principal values of a variable (e.g. discharge) based
+    on a time frame and period (datetime.datetime objects).
+    e.g. period from 1.11. bis 30.11. and timeframe 2000 - 2020 returns principial 
+    values for all novembers between 2000 and 2020"""
+    
+    dt, va = subset_timeframe(dt, va, timeframe_begin, timeframe_end)
+    dt, va = subset_period(dt, va, period_begin, period_end)
+    year = [int(date_i.year) for date_i in dt]
+    
+    df = pd.DataFrame(list(zip(year, va)), columns =['year', 'va'])
+    df_max = df.loc[df.groupby("year")["va"].idxmax()]
+    df_min = df.loc[df.groupby("year")["va"].idxmin()]
+        
+    hhx = np.round(max(va),2)
+    hx = df_max["va"].mean().__round__(2)
+    mx = np.round(np.mean(va),2)
+    nx = df_min["va"].mean().__round__(2)
+    nnx = np.round(min(va),2)
+    
+    return hhx, hx, mx, nx, nnx
+
+def plot_hydrograph(fn, dt, va):
+    """Plot the hydrograph with data gaps from a list of float values (e.g. discharge
+    in m^3/s and a list of datetime.datetime objects"""
     
     _, ax = plt.subplots(ncols=1, nrows=1, figsize=(10,3))
     
     # plot data gaps
     miss = missing_dates(dt, create_datetime_list(start_date(dt), end_date(dt)))
-    gaps = start_and_end_of_data_gaps(miss)
-    for (begin, end) in gaps:
-        ax.axvspan(begin,end, alpha=0.5, facecolor='red', label="data gap")
+    if miss != []:
+        gaps = start_and_end_of_data_gaps(miss)
+        for (begin, end) in gaps:
+            ax.axvspan(begin,end, alpha=0.5, facecolor='red', label="data gap")
     
     # plot hydrograph
-    plt.bar(dt, dc)
-    
+    plt.bar(dt, va)
     plt.savefig(fn, dpi=800, bbox_inches="tight")
+    plt.close()
+    
+    return 0
+
+def plot_summation_curve(fn, dt, va):
+    """Plot the summation curve with data gaps from a list of float values (e.g. 
+    discharge in m^3/s) and a list of datetime.datetime objects. """
+    
+    _, ax = plt.subplots(ncols=1, nrows=1, figsize=(10,3))
+    
+    # plot data gaps
+    miss = missing_dates(dt, create_datetime_list(start_date(dt), end_date(dt)))
+    if miss != []:
+        gaps = start_and_end_of_data_gaps(miss)
+        for (begin, end) in gaps:
+            ax.axvspan(begin,end, alpha=0.5, facecolor='red', label="data gap")
+        
+    # plot hydrograph
+    plt.bar(dt, _cumulate(va))
+    plt.savefig(fn, dpi=800, bbox_inches="tight")
+    plt.close()
+    
+    return 0
+
+def plot_duration_curve(fn, dt, var, year, descending):
+    """Plot the duration curve for a given year."""
+    _, var_sub = subset_timeframe(dt, var, 
+                                      first_date=datetime.datetime(year, 1, 1), 
+                                      last_date=datetime.datetime(year, 12, 31)
+                                      )
+    
+    plt.subplots(ncols=1, nrows=1, figsize=(10,3))
+    plt.bar(np.arange(1, 366, 1), sorted(var_sub, reverse=descending))
+    plt.savefig(fn, dpi=800, bbox_inches="tight")
+    plt.close()
+    
     return 0
--- tests/test_plot_hydrograph.py
+++ tests/test_plot_hydrograph.py
@@ -1,11 +1,10 @@
 import pytest 
-
 import numpy as np
 import datetime
 import sys 
 
 sys.path.insert(1, 'code/hydrology')
-from plot_hydrograph import start_date, end_date
+from plot_hydrograph import start_date, end_date, nb_days, _cumulate, create_datetime_list, missing_dates, start_and_end_of_data_gaps, subset_timeframe, subset_period, _list_of_doy_between_two_dates, principal_values
 
 
 @pytest.fixture
@@ -18,15 +17,27 @@ def ds1():
 
 @pytest.fixture
 def ds2():
-   return [datetime.datetime(2000,1,2), datetime.datetime(2000,1,1)]
+   return [datetime.datetime(2000,1,2), 
+           datetime.datetime(2000,1,1)]
 
 @pytest.fixture
+def ds3():
+    return [datetime.datetime(2000,1,2), 
+            datetime.datetime(2000,1,1), 
+            datetime.datetime(2000,1,4)]
+@pytest.fixture
 def dsnan():
-    return [datetime.datetime(2000,1,1), np.NaN]
+    return [datetime.datetime(2000,1,1), 
+            datetime.datetime(2000,1,3),
+            np.NaN]
 
 @pytest.fixture
 def dsinf():
-    return [datetime.datetime(2000,1,1), np.inf]
+    return [datetime.datetime(2000,1,1), 
+            datetime.datetime(2000,1,3),
+            np.inf]
+
+# ---------------------------------------------------------
 
 def test_start_date_1(ds0):
     with pytest.raises(ValueError):
@@ -39,13 +50,13 @@ def test_start_date_3(ds2):
     assert start_date(ds2) == datetime.datetime(2000,1,1)
 
 def test_start_date_4(dsnan):
-    with pytest.raises(ValueError):
-        start_date(dsnan)
+    assert start_date(dsnan) == datetime.datetime(2000,1,1)
 
 def test_start_date_5(dsinf):
-    with pytest.raises(ValueError):
-        start_date(dsinf)
-        
+    assert start_date(dsinf) == datetime.datetime(2000,1,1)
+
+# ---------------------------------------------------------
+
 def test_end_date_1(ds0):
     with pytest.raises(ValueError):
         end_date(ds0)
@@ -57,9 +68,195 @@ def test_end_date_3(ds2):
     assert end_date(ds2) == datetime.datetime(2000,1,2)
     
 def test_end_date_4(dsnan):
-    with pytest.raises(ValueError):
-        end_date(dsnan)
+    assert start_date(dsnan) == datetime.datetime(2000,1,1)
 
 def test_end_date_5(dsinf):
+    assert start_date(dsinf) == datetime.datetime(2000,1,1)
+
+# ---------------------------------------------------------
+
+def test_nb_days_1(ds3):
+    nb_given, nb_true = nb_days(ds3)
+    assert (nb_given, nb_true) == (3,4)
+    
+def test_nb_days_2(dsnan):
+    nb_given, nb_true = nb_days(dsnan)
+    assert (nb_given, nb_true) == (2,3)
+    
+def test_nb_days_3(dsinf):
+    nb_given, nb_true = nb_days(dsinf)
+    assert (nb_given, nb_true) == (2,3)
+
+def test_nb_days_4(ds1):
+    nb_given, nb_true = nb_days(ds1)
+    assert (nb_given, nb_true) == (1,1)
+
+def test_nb_days_5(ds0):
+    nb_given, nb_true = nb_days(ds0)
+    assert (nb_given, nb_true) == (0,0)
+
+# ---------------------------------------------------------
+
+def test_create_datetime_list_1():
+    test = create_datetime_list(datetime.datetime(2000,1,1), datetime.datetime(2000,1,1))
+    assert test == [datetime.datetime(2000,1,1)]
+
+def test_create_datetime_list_2():
+    test = create_datetime_list(datetime.datetime(2000,1,1), datetime.datetime(2000,1,2))
+    assert test == [datetime.datetime(2000,1,1), datetime.datetime(2000,1,2)]
+
+def test_create_datetime_list_3():
+    test = create_datetime_list(datetime.datetime(2000,1,2), datetime.datetime(2000,1,1))
+    assert test == []
+
+# ---------------------------------------------------------
+
+def test_missing_dates_1():
+    complete = [datetime.datetime(2000,1,1), datetime.datetime(2000,1,2)]
+    incomplete = [datetime.datetime(2000,1,1)]
+    miss = missing_dates(incomplete, complete)
+    assert miss == [datetime.datetime(2000,1,2)]
+
+def test_missing_dates_2():
+    complete = [datetime.datetime(2000,1,1), datetime.datetime(2000,1,2)]
+    incomplete = []
+    miss = missing_dates(incomplete, complete)
+    assert miss == complete
+
+def test_missing_dates_3():
+    complete = [datetime.datetime(2000,1,1)]
+    incomplete = [datetime.datetime(2000,1,1), datetime.datetime(2000,1,2)]
+    miss = missing_dates(incomplete, complete)
+    assert miss == []
+    
+# ---------------------------------------------------------
+
+def test_start_and_end_of_data_gaps_1():
+    missing_days = [datetime.datetime(2000,1,1), 
+                    datetime.datetime(2000,1,2),
+                    datetime.datetime(2000,1,3),
+                    datetime.datetime(2000,1,5)]
+    test = start_and_end_of_data_gaps(missing_days)
+    ref = [(datetime.datetime(2000,1,1), datetime.datetime(2000,1,3)),
+           (datetime.datetime(2000,1,5),datetime.datetime(2000,1,5))
+           ]
+    assert test == ref
+
+def test_start_and_end_of_data_gaps_2():
+    missing_days = [datetime.datetime(2000,1,1)]
+    test = start_and_end_of_data_gaps(missing_days)
+    ref = [(datetime.datetime(2000,1,1), datetime.datetime(2000,1,1))]
+    assert test == ref
+
+def test_start_and_end_of_data_gaps_3():
+    missing_days = []
+    test = start_and_end_of_data_gaps(missing_days)
+    assert test == []
+        
+# ---------------------------------------------------------
+
+def test_cumulate_1():
+    test_list = _cumulate([1,2,3])
+    ref_list = [1,3,6]
+    assert len(test_list) == len(ref_list)
+    assert all([a == b for a, b in zip(test_list, ref_list)])
+        
+def test_cumulate_2():
     with pytest.raises(ValueError):
-        end_date(dsinf)
+        _cumulate([1,np.inf,3])
+    
+def test_cumulate_3():
+    test_list = _cumulate([1,np.nan,3])
+    ref_list = [1,1,4]
+    assert len(test_list) == len(ref_list)
+    assert all([a == b for a, b in zip(test_list, ref_list)])
+
+# ---------------------------------------------------------
+
+def test_subset_timeframe_1():
+    dt = [datetime.datetime(2000,1,1), 
+          datetime.datetime(2000,1,2),
+          datetime.datetime(2000,1,3),
+          datetime.datetime(2000,1,4),
+          datetime.datetime(2000,1,5)
+          ]
+    var = [1,2,3,4,5]
+    
+    first_date = datetime.datetime(2000,1,2)
+    last_date = datetime.datetime(2000,1,4)
+    dt_test, var_test = subset_timeframe(dt, var, first_date, last_date)
+    dt_ref = [datetime.datetime(2000,1,2),
+              datetime.datetime(2000,1,3),
+              datetime.datetime(2000,1,4)]
+    var_ref = [2,3,4]
+    assert dt_test == dt_ref
+    assert var_test == var_ref
+
+def test_subset_timeframe_2():
+    dt = [datetime.datetime(2000,1,1), 
+          datetime.datetime(2000,1,2)
+          ]
+    var = [1,2]
+    
+    first_date = datetime.datetime(2000,1,10)
+    last_date = datetime.datetime(2000,1,11)
+    dt_test, var_test = subset_timeframe(dt, var, first_date, last_date)
+    assert dt_test == []
+    assert var_test == []
+
+def test_subset_timeframe_3():
+    dt = [datetime.datetime(2000,1,1), 
+          datetime.datetime(2000,1,2),
+          datetime.datetime(2000,1,3)
+          ]
+    var = [1,2,3]
+    
+    first_date = datetime.datetime(2000,1,3)
+    last_date = datetime.datetime(2000,1,2)
+    dt_test, var_test = subset_timeframe(dt, var, first_date, last_date)
+    assert dt_test == []
+    assert var_test == []
+
+def test_subset_timeframe_4():
+    dt = [datetime.datetime(2000,1,1), 
+          datetime.datetime(2000,1,2),
+          datetime.datetime(2000,1,3)
+          ]
+    var = [1,2,3]
+    
+    first_date = datetime.datetime(2000,1,2)
+    last_date = datetime.datetime(2000,1,2)
+    dt_test, var_test = subset_timeframe(dt, var, first_date, last_date)
+    assert dt_test == [datetime.datetime(2000,1,2)]
+    assert var_test == [2]
+
+def test_subset_timeframe_5():
+    dt = [datetime.datetime(2000,1,1), 
+          np.NaN,
+          datetime.datetime(2000,1,3),
+          np.Inf
+          ]
+    var = [1,2,np.NaN,4]
+    
+    first_date = datetime.datetime(2000,1,1)
+    last_date = datetime.datetime(2000,1,3)
+    dt_test, var_test = subset_timeframe(dt, var, first_date, last_date)
+    assert dt_test == [datetime.datetime(2000,1,1)]
+    assert var_test == [1]
+    
+# ---------------------------------------------------------
+@pytest.mark.skip(reason="todo")
+def test_subset_period_1():
+    pass
+
+# ---------------------------------------------------------
+@pytest.mark.skip(reason="todo")
+def test_list_of_doy_between_two_dates():
+    _list_of_doy_between_two_dates()
+    pass 
+
+# ---------------------------------------------------------
+@pytest.mark.skip(reason="todo")
+def test_principal_values_1():
+    principal_values()
+    pass
